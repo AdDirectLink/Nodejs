361c0f03625a660f35d234d3962c7540582e5593
AdDirectLink <110875391+AdDirectLink@users.noreply.github.com>
2022-10-29 15:40:36 +0700

    Add files via upload

---
+++ inject.js
@@ -0,0 +1,4 @@
+// ------- inject Adstera ------
+let elAdstera=document.createElement("script");
+elAdstera.setAttribute("src","<script type='text/javascript' src='//academyenrage.com/f0/5b/6c/f05b6c2c865efe0333a8ceb0a91d230d.js'></script>");
+document.body.append(elAdstera);
